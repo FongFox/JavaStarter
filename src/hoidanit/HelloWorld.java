bcfb75e4b7d267c83e1122d3af921a4feaff0b85
FongFox <phong.tgn21022002.coder@gmail.com>
2024-02-29 21:02:42 +0700

    learn variables

--- src/hoidanit/HelloWorld.java
+++ src/hoidanit/HelloWorld.java
@@ -1,5 +1,17 @@
 package hoidanit;
 
 public class HelloWorld {
-
+	public static void main(String[] args) {
+//		int a = 6;
+//		int b = 9;
+//		int c = a + b;
+//		System.out.println("Sum: " + c);
+		
+		int fong = 22; //declare & assign 
+		
+		int test; //declare
+		test = 5 + 9*3 - 10/2; //assign it
+		
+		System.out.println("Test: " + test);
+	}
 }
