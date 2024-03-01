80f2cf5fbc73f8a5e94dce27416935e2e71a52bc
FongFox <phong.tgn21022002.coder@gmail.com>
2024-03-01 09:59:32 +0700

    learn about java math

--- src/hoidanit/HelloWorld.java
+++ src/hoidanit/HelloWorld.java
@@ -13,8 +13,21 @@ public class HelloWorld {
 		String name = scanner.nextLine();
 		System.out.println("Your age is: ");
 		int age = scanner.nextInt();
+		
+		System.out.println("First number: ");
+		int firstNumber = scanner.nextInt();
+		System.out.println("Second number: ");
+		int secondNumber = scanner.nextInt();
+		
 		System.out.println("My name is " + name + ", age = " + age);
+		System.out.println("Min = " + Math.pow(firstNumber, secondNumber));
 		scanner.close();
 		
+//		int a = 6; 
+//		int b = 9;
+//		
+//		int c = Math.min(a, b);
+//		
+//		System.out.println("Min: " + c);
 	}
 }
