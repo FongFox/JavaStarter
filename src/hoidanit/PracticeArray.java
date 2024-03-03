8cac93a87eab6d853ffafb3d6cfc4a777766f209
FongFox <phong.tgn21022002.coder@gmail.com>
2024-03-03 15:28:11 +0700

    init and add code to project for practice basic array

---
+++ src/hoidanit/PracticeArray.java
@@ -0,0 +1,27 @@
+package hoidanit;
+
+public class PracticeArray {
+	public static void main(String[] args) {
+//		String[] clubs = { "MU", "Mancity", "Liverpool" };
+		int[] clubs = { 1, 10, 5, 16, 3 };
+
+//		Arrays.sort(clubs);
+//
+//		System.out.println(Arrays.toString(clubs)); // conver array duoi dang String
+
+//		System.out.println(Arrays.sort(clubs));
+
+//		for (int i = 0; i < clubs.length - 1; i++) {
+//			for (int k = i + 1; k < clubs.length; k++) {
+//				if (clubs[i] > clubs[k]) {
+//					int temp = clubs[i];
+//					clubs[i] = clubs[k];
+//					clubs[k] = temp;
+//				}
+//			}
+//		}
+//
+//		System.out.println(Arrays.toString(clubs));
+
+	}
+}
