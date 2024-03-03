ecf8b6033379cbda327aaa376b5d4c02c7a02c9f
FongFox <phong.tgn21022002.coder@gmail.com>
2024-03-03 15:15:51 +0700

    init and add code to project learn about basic concept of array

---
+++ src/hoidanit/LearnArray.java
@@ -0,0 +1,26 @@
+package hoidanit;
+
+public class LearnArray {
+	public static void main(String[] args) {
+//		String[] clubs = { "MU", "Mancity", "Liverpool" };
+//		clubs[0] = "Arsenal";
+
+//		System.out.println("run here array with length: " + clubs.length);
+
+//		System.out.println("run 0 element array: " + clubs[0]);
+//		System.out.println("run 1 element array: " + clubs[6]);
+
+//		System.out.println("run here array: " + Arrays.toString(clubs));
+
+//		String[] a = new String[5];
+//		a[0] = "1";
+//
+//		System.out.println("run 1 element array: " + a[1]);
+
+		String[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+		for (int i = 0; i < cars.length; i++) {
+			System.out.println(cars[i]);
+		}
+
+	}
+}
